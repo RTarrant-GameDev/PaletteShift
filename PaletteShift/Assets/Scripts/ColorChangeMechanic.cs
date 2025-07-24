f66fa146ca8b2924b49857157114450309a56f85
ReeceTarrant <rtarrant70998@gmail.com>
2025-07-24 14:58:27 +0100

    Created duplicate of sprite for use with NPCs (if I were to add them to the game) + extended color change mechanics by splitting into two scripts: One (Mechanic) to handle actual color change, and the other (Manager) to keep track of both color to change to and duration of change before player sprite reverts back to original color.

--- PaletteShift/Assets/Scripts/ColorChangeMechanic.cs
+++ PaletteShift/Assets/Scripts/ColorChangeMechanic.cs
@@ -2,41 +2,23 @@ using UnityEngine;
 
 public class ColorChangeMechanic: MonoBehaviour
 {
-    SpriteRenderer playerCharacterSpriteRenderer;
-
-    [SerializeField]
-    Color newColor;
-
-    [SerializeField]
-    Color defaultColor;
+    SpriteRenderer PlayerCharacterSpriteRenderer;
 
     void Start() {
-        playerCharacterSpriteRenderer = GetComponent<SpriteRenderer>();
-    
-        defaultColor = Color.black;
+        PlayerCharacterSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void Update() {
-        if (Input.GetKeyUp(KeyCode.J)){
-            ChangeColour();
-        }
-    }
-
-    void ChangeColour(){
+    public void ChangeColor(Color TargetColor){
 
         // Sherlock Comment: Store texture of actual sprite, loop through pixels in texture, check if each pixel actually contains a color, 
         // and if it's black, change; if new color, revert back to black
 
-        Texture2D StickmanTexture = playerCharacterSpriteRenderer.sprite.texture;
+        Texture2D StickmanTexture = PlayerCharacterSpriteRenderer.sprite.texture;
 
         for(int y = 0; y < StickmanTexture.height; y++) {
             for (int x = 0; x < StickmanTexture.width; x++) {
-                    if(StickmanTexture.GetPixel(x, y) != new Color(0.000f, 0.000f, 0.000f, 0.000f)) {
-                        if(StickmanTexture.GetPixel(x, y) == defaultColor) {
-                        StickmanTexture.SetPixel(x, y, newColor);
-                    } else {
-                        StickmanTexture.SetPixel(x, y, defaultColor);
-                    }
+                if(StickmanTexture.GetPixel(x, y) != new Color(0.000f, 0.000f, 0.000f, 0.000f)) {
+                    StickmanTexture.SetPixel(x, y, TargetColor);
                 }
             }      
         }
