9f561dc63f4104ac569742d8c25626a2cfd17a17
ReeceTarrant <rtarrant70998@gmail.com>
2025-07-26 21:18:49 +0100

    Implemented FiniteStateMachine to manage events that occur when PlayerState has changed (i.e. if player is idle, moving, jumping, etc).

--- PaletteShift/Assets/Scripts/Player/PlayerController.cs
+++ PaletteShift/Assets/Scripts/Player/PlayerController.cs
@@ -20,48 +20,61 @@ public class PlayerController : MonoBehaviour
     private bool IsGrounded;
     private bool JumpPressed;
 
-    private void Awake()
-    {
+    [Header("ExternalComponents")]
+    private FiniteStateMachine PlayerMovementFSM;
+
+    private void Awake() {
         PlayerRB = GetComponent<Rigidbody2D>();
+        PlayerMovementFSM = GetComponent<FiniteStateMachine>();
 
         MoveAction = InputSystem.actions.FindAction("Move");
         JumpAction = InputSystem.actions.FindAction("Jump");
     }
 
-    private void Update()
-    {
+    private void Update() {
         // Check if grounded
         IsGrounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, GroundLayer);
 
-        if (JumpAction != null && JumpAction.WasPressedThisFrame() && IsGrounded)
-        {
+        if (JumpAction != null && JumpAction.WasPressedThisFrame() && IsGrounded) {
             JumpPressed = true;
+            
         }
     }
 
-    private void FixedUpdate()
-    {
-        if (MoveAction != null)
-        {
+    private void FixedUpdate() {
+        if (MoveAction != null) {
             // Move player
             MoveInput = MoveAction.ReadValue<Vector2>();
+
+            if (MoveInput != Vector2.zero)
+            {
+                PlayerMovementFSM.ChangeState("Move");
+            }
+            else if (MoveInput == Vector2.zero)
+            {
+                PlayerMovementFSM.ChangeState("Idle");
+            }
+
             PlayerRB.linearVelocity = new Vector2(MoveInput.x * MoveSpeed, PlayerRB.linearVelocity.y);
-            
         }
+
+
+
 
         // Jump
         if (JumpPressed && IsGrounded)
         {
             PlayerRB.linearVelocity = new Vector2(PlayerRB.linearVelocity.x, JumpForce);
         }
+        else if (!IsGrounded) {
+            PlayerMovementFSM.ChangeState("Jump");
+        }
 
         JumpPressed = false;
     }
 
-    private void OnDrawGizmosSelected()
-    {
-        if (GroundCheck != null)
-        {
+    private void OnDrawGizmosSelected() {
+        if (GroundCheck != null) {
             Gizmos.color = Color.green;
             Gizmos.DrawWireSphere(GroundCheck.position, GroundCheckRadius);
         }
