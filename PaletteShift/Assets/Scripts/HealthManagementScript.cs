a45b330b8e4c5ff47951cd0f44e85b29a877a75e
ReeceTarrant <rtarrant70998@gmail.com>
2025-08-06 16:08:14 +0100

    Updated script to allow setting of Health from editor + generally changed around stuff in sample scene + fixed issue where colliding with collectable was making player fall over.

--- PaletteShift/Assets/Scripts/HealthManagementScript.cs
+++ PaletteShift/Assets/Scripts/HealthManagementScript.cs
@@ -1,26 +1,17 @@
 using UnityEngine;
 
-public class HealthManagementScript : MonoBehaviour
-{
+public class HealthManagementScript : MonoBehaviour {
     private HealthScript HealthModel;
+    [SerializeField] private int HealthValue;
     [SerializeField] private HealthUIScript HealthView;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        HealthModel = new HealthScript(10);
+    void Start() {
+        HealthModel = new HealthScript(HealthValue);
         HealthModel.OnHealthChanged += HealthView.SetHealth;
 
         //Initial sync
         HealthView.SetHealth(HealthModel.CurrentHealth, HealthModel.MaxHealth);
-
-        //Simulate damage
-        InvokeRepeating(nameof(SimulateDamage), 2f, 2f);
-    }
-
-    void SimulateDamage()
-    {
-        HealthModel.TakeDamage(Random.Range(1, 3));
     }
 
     public void HealPlayer(int amount) => HealthModel.Heal(amount);
