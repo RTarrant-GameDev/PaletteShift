1e24385d9c751fd36330116185a31bdba2943528
ReeceTarrant <rtarrant70998@gmail.com>
2025-08-22 00:25:24 +0100

    Created dedicated GameSystemManager script for organising game systems (and prepping for use when starting new levels) + expanded UI to include a mission timer and as well as UI to display the timer before the player's color changes back to normal

---
+++ PaletteShift/Assets/Scripts/ColorChangeUIScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChangeUIScript : MonoBehaviour {
+    public TextMeshProUGUI ColorTimerText;
+    public Image ColorTargetImage;
+
+    void Start()
+    {
+        if (ColorChangeManager.ManagerInstance != null)
+        {
+            ColorChangeManager.ManagerInstance.OnColorChange += HandleColorChange;
+        }
+
+        SetUIElementVisibility(false); //have UI elements hidden at start
+    }
+
+    private void HandleColorChange(Color colorToChange, float colorChangeTime)
+    {
+        StartCoroutine(ColorChangeUIUpdate(colorToChange, colorChangeTime));
+    }
+
+    IEnumerator ColorChangeUIUpdate(Color ColorToChange, float ColorChangeTime)
+    {
+
+        SetUIElementVisibility(true);
+
+        float timeLeft = ColorChangeTime;
+
+        // Optionally, set the color once at the beginning
+        if (ColorTargetImage != null)
+            ColorTargetImage.color = ColorToChange;
+
+        while (timeLeft > 0f)
+        {
+            timeLeft -= Time.deltaTime; // Reduce time by deltaTime each frame
+
+            // If you want to display the countdown
+            if (ColorTimerText != null)
+                ColorTimerText.text = Mathf.Ceil(timeLeft).ToString();
+
+            yield return null; // Wait until next frame
+        }
+
+        // After timer ends, reset or do something
+        if (ColorTimerText != null)
+            ColorTimerText.text = "0";
+
+        SetUIElementVisibility(false);
+    }
+
+    // Created dedicated function for changing UI visibility to allow for better organisation
+    void SetUIElementVisibility(bool ShouldBeActive) {
+        ColorTargetImage.enabled = ShouldBeActive;
+        ColorTimerText.enabled = ShouldBeActive;
+    }
+}
---
+++ PaletteShift/Assets/Scripts/GameSystemManagerScript.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class GameSystemManagerScript : MonoBehaviour {
+    // Kick off essential game systems on Start (StartTimer, etc)
+    void Start() {
+        MissionTimer.TimerInstance.StartTimer();
+    }
+}
