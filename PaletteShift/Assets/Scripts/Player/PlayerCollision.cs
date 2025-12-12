4293607b479932dbe549d41d898fadd3f78aac49
ReeceTarrant <rtarrant70998@gmail.com>
2025-12-12 15:06:41 +0000

    Created tutorial hint object + relocated ColorChangeUI script to ColorChange folder

--- PaletteShift/Assets/Scripts/Player/PlayerCollision.cs
+++ PaletteShift/Assets/Scripts/Player/PlayerCollision.cs
@@ -6,6 +6,8 @@ public class PlayerCollision : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.GetComponent<EndpointScript>()){
             collision.gameObject.GetComponent<EndpointScript>().EndLevel();
+        } else if (collision.gameObject.GetComponent<TutorialHintScript>()) {
+            CanvasManager.CanvasManagerInstance.DisplayTutorialText(collision.gameObject.GetComponent<TutorialHintScript>().TutorialHintText);
         }
     }
 }
